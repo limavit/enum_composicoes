01e2df9813c50b6e0575c84dc310c41522ff52c1
Vitor Lima <limavit@gmail.com>
2021-05-04 12:14:38 -0300

    Classes entities

--- src/entities/Worker.java
+++ src/entities/Worker.java
@@ -1,5 +1,9 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 import entities.enums.Worklevel;
 
 public class Worker {
@@ -7,15 +11,78 @@ public class Worker {
 	private Worklevel level;
 	private Double baseSalary;
 	
-	public Worker () {
+	private Department department;
+	private List<HourContract> contracts = new ArrayList<>();
+	
+	public Worker() {
 		
 	}
 
-	public Worker(String name, Worklevel level, Double baseSalary) {
+	public Worker(String name, Worklevel level, Double baseSalary, Department department) {
+		super();
 		this.name = name;
 		this.level = level;
 		this.baseSalary = baseSalary;
+		this.department = department;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Worklevel getLevel() {
+		return level;
+	}
+
+	public void setLevel(Worklevel level) {
+		this.level = level;
+	}
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public List<HourContract> getContracts() {
+		return contracts;
+	}	
+	
+	public void addContract(HourContract contract) {
+		contracts.add(contract);
+	}
+	public void remove(HourContract contract) {
+		contracts.remove(contract);
 	}
 	
+	public Double income(int year, int month) {
+		double sum = baseSalary;
+		Calendar cal = Calendar.getInstance();
+		for (HourContract c : contracts) {
+			cal.setTime(c.getDate());
+			int c_year = cal.get(Calendar.YEAR);
+			int c_month = 1 + cal.get(Calendar.MONTH);
+			if (year == c_year && month == c_month) {
+				sum += c.totalValue();
+				
+			}
+		}
+		
+		return sum;
+	}
 
 }
